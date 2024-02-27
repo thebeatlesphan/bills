b4bc177bc0f95add584f33a98777daffdc3c4bf1
Andy <aphanit@protonmail.com>
2024-02-26 23:34:33 -0500

    Revert "Removed Controllers and Using RepositoryRestResource to automatically…"

--- src/main/java/com/example/bills/user/User.java
+++ src/main/java/com/example/bills/user/User.java
@@ -1,6 +1,5 @@
 package com.example.bills.user;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,7 +10,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(unique=true)
     private String username;
     private String password;
 
--- src/main/java/com/example/bills/user/UserRepository.java
+++ src/main/java/com/example/bills/user/UserRepository.java
@@ -1,12 +1,7 @@
 package com.example.bills.user;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "user", path = "user")
-public interface UserRepository extends PagingAndSortingRepository<User, Integer>, CrudRepository<User, Integer>{
+public interface UserRepository extends CrudRepository<User, Integer>{
     
-    User findByUsername(@Param("username") String username);
 }
