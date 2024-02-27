5e608c70a08cd97646c9de2e00d8b5e02d211eae
Andy <aphanit@protonmail.com>
2024-02-26 23:34:33 -0500

    Revert "Removed Controllers and Using RepositoryRestResource to automatically…"

--- src/main/java/com/example/bills/clan/ClanRepository.java
+++ src/main/java/com/example/bills/clan/ClanRepository.java
@@ -1,14 +1,7 @@
 package com.example.bills.clan;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "clan", path = "clan")
-public interface ClanRepository extends PagingAndSortingRepository<Clan, Integer>, CrudRepository<Clan, Integer> {
+public interface ClanRepository extends CrudRepository<Clan, Integer> {
 
-    List<Clan> findByClanName(@Param("clanName") String clanName);
 }
